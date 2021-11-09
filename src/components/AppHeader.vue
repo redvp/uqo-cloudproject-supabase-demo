eee46617a5288e1094624c88768d6f260b2e3a11
redvp <vincent.perraud@icloud.com>
2021-11-09 14:32:49 -0500

    feat: added logout function

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,19 +6,38 @@
     <router-link :to="{ name: 'home' }">
       <h3 className="mx-2">Supabase Demo ToDo List</h3>
     </router-link>
-    <h1 v-if="username" className="mx-2">Hello {{ username }}</h1>
+    <div
+      v-if="username.length"
+      className="flex flex-row justify-between items-center"
+    >
+      <h1 className="mx-2">Hello {{ username }}</h1>
+      <button
+        className="mx-2 border-5 rounded bg-blue-500 px-2 py-1 text-white"
+        @click="logout"
+      >
+        Logout
+      </button>
+    </div>
     <router-link v-else :to="{ name: 'login' }">
-      <h2 className="mx-2 border-5 rounded bg-blue-600 px-4 py-2">Sign In</h2>
+      <h2 className="mx-2 border-5 rounded bg-green-500 px-2 py-1">Sign In</h2>
     </router-link>
   </div>
 </template>
 
 <script>
+import { inject } from '@vue/runtime-core'
+import router from '../router'
+
 export default {
-  props: {
-    username: {
-      type: String,
-      default: ''
+  props: { username: { type: String, default: '' } },
+  setup() {
+    const store = inject('store')
+    function logout() {
+      store.state.user = {}
+      router.push({ name: 'login' })
+    }
+    return {
+      logout
     }
   }
 }
